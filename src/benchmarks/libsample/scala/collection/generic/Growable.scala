912aaed1ca9c925508c1d750cdf9e3c79926a9a0
Cristian Tălău <cristitalau@gmail.com>
2012-04-27 13:23:13 +0200

    moved library sample to another project

--- src/benchmarks/libsample/scala/collection/generic/Growable.scala
+++
@@ -1,9 +0,0 @@
-
-package benchmarks.libsample.scala.collection.generic
-import benchmarks.libsample.scala.collection.immutable._
-
-trait Growable[-A] {
-  def +=(elem: A): this.type
-  def ++=(xs: List[A]): this.type = { xs foreach += ; this }
-  def clear()
-}
--- src/benchmarks/libsample/scala/collection/mutable/HashEntry.scala
+++
@@ -1,6 +0,0 @@
-package benchmarks.libsample.scala.collection.mutable
-
-trait HashEntry [A, E] {
-  val key: A
-  var next: E = _
-}
